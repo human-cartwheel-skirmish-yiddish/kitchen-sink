f8f5a8e12c043ffd00615eb7d97f7d83b0d96020
uyennhiquang <111651301+uyennhiquang@users.noreply.github.com>
2026-09-25 23:32:53 -0400

    Added code sample that queries from db

--- main.py
+++ main.py
@@ -1,17 +1,16 @@
 import psycopg
 
-
 def main():
-  with psycopg.connect("dbname=dvdrental user=uq8273") as conn:
+  with psycopg.connect("dbname=kitchen_sink user=uq8273") as conn:
 
       # Open a cursor to perform database operations
       with conn.cursor() as cur:
 
           # You can use `cur.fetchmany()`, `cur.fetchall()` to return a list
           # of several records, or even iterate on the cursor
-          cur.execute("SELECT (first_name || ' ' || last_name) FROM customer")
+          cur.execute("SELECT (recipes).ingredients FROM recipe_col")
           for record in cur:
-              print(record[0])
+              print(record[0][0])
 
           # Make the changes to the database persistent
           conn.commit()
